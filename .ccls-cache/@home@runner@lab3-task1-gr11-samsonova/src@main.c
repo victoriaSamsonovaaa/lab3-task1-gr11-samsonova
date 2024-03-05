acbf80dbb83f48860c11a707dd69bb495865a9f4
VIKTORYIA SAMSONAVA <140270932+victoriaSamsonovaaa@users.noreply.github.com>
2024-03-05 20:12:02 +0000

    adding code

--- .ccls-cache/@home@runner@lab3-task1-gr11-samsonova/src@main.c
+++ .ccls-cache/@home@runner@lab3-task1-gr11-samsonova/src@main.c
@@ -1,7 +1,39 @@
-/* hello.c */
 #include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
 
-void main(void) {
-  printf("Hello World\n");
-  printf("Goodbye World\n");
+int exactSquare(int number) {
+  int squareRoot = sqrt(number);
+  return squareRoot * squareRoot == number;
 }
+
+void printS(int arr[], int n) {
+  printf("Exact squares in array: ");
+  for (int i = 0; i < n; i++) {
+    if (exactSquare(arr[i])) {
+      printf("%d ", arr[i]);
+      printf(" ");
+    }
+  }
+  printf("\n");
+}
+
+
+int main(int argc, char* argv[]) {
+  int n;
+  printf("Input size of an array: \n");
+  scanf("%d", &n);
+  int array[n];
+
+  srand(time(NULL));
+  printf("Your array:\n");
+  for (int i = 0; i < n; i++) {
+      array[i] = rand() % 1;
+      printf("%d ", array[i]);
+        }
+        printf("\n");
+
+        printS(array, n);
+
+    }
